f3967afc0622f384e8c41d45a08f02ee2d95bdcf
Brian Monroe <58818058+brianmonroe@users.noreply.github.com>
2025-05-19 14:02:36 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'site_title' => 'Event Watch',
+    'logo_path' => 'logo.png', // Place a logo in the same directory
+    'events' => [
+        ['date' => '20250601', 'title' => 'Summer Music Fest', 'type' => 'music'],
+        ['date' => '20250525', 'title' => 'Local Baseball Game', 'type' => 'sports'],
+        ['date' => '20250615', 'title' => 'Community Picnic', 'type' => 'other'],
+        ['date' => '20250415', 'title' => 'Old Concert', 'type' => 'music'], // Will be skipped
+    ]
+];
